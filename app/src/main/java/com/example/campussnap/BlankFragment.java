9e3f078865ba05010ff1bf8ae4484d1914ca6bae
Kelly Hong <411487840@qq.com>
2021-10-26 22:07:15 +0800

    feat:完善导航逻辑功能

---
+++ app/src/main/java/com/example/campussnap/BlankFragment.java
@@ -0,0 +1,67 @@
+package com.example.campussnap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+
+    private static final String ARG_TEXT = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mTextString;
+    View rootView;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment newInstance(String text) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mTextString = getArguments().getString(ARG_TEXT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if(rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+        }
+        initView();
+        return rootView;
+    }
+
+    private void initView() {
+        TextView textView = rootView.findViewById(R.id.text);
+        textView.setText(mTextString);
+    }
+}

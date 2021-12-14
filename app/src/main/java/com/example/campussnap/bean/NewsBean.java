0160d0559f2b61fdee5c322022443e5c055d0b31
hocztms <1107578936@qq.com>
2021-12-14 21:40:42 +0800

    新闻列表网络对接

---
+++ app/src/main/java/com/example/campussnap/bean/NewsBean.java
@@ -0,0 +1,20 @@
+package com.example.campussnap.bean;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsBean {
+
+    private Integer id;
+    private String imageUrl;
+    private String title;
+    private String desc;
+    private String publishAccount;
+    private Date publishTime;
+}
--- app/src/main/java/com/example/campussnap/fragment/NewsFragment.java
+++ app/src/main/java/com/example/campussnap/fragment/NewsFragment.java
@@ -1,60 +1,73 @@
 package com.example.campussnap.fragment;
 
 import android.content.Context;
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.alibaba.fastjson.JSONArray;
 import com.bumptech.glide.Glide;
 import com.example.campussnap.R;
+import com.example.campussnap.bean.NewsBean;
 import com.example.campussnap.common.AppContext;
-import com.example.campussnap.config.ListItem;
+import com.example.campussnap.common.Result;
+import com.example.campussnap.config.NewsVo;
+import com.example.campussnap.utils.HttpUtils;
 import com.example.campussnap.utils.LogUtils;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class NewsFragment extends Fragment {
 
     private TitleAdapter titleAdapter;
-    private RecyclerView mRecyclerView;
-    private ArrayList<ListItem> list = new ArrayList<>();    // 新闻列表数据
+    private RecyclerView mRecyclerView;// 新闻列表数据
+    private List<NewsBean> newsBeanList = new ArrayList<>();
+    private DateFormat dateFormat;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        initList();
         View newsView = inflater.inflate(R.layout.fragment_news, container, false);
         mRecyclerView=newsView.findViewById(R.id.lv_news);
 
-        titleAdapter = new TitleAdapter(AppContext.getInstance(), list);
+        titleAdapter = new TitleAdapter(AppContext.getInstance());
         mRecyclerView.setLayoutManager (new LinearLayoutManager(AppContext.getInstance(),LinearLayoutManager.VERTICAL,false));
-//        mRecyclerView.setItemAnimator (new DefaultItemAnimator());
         mRecyclerView.setAdapter (titleAdapter);
+
+//        mRecyclerView.setItemAnimator (new DefaultItemAnimator());
 //        mRecyclerView.addItemDecoration (new DividerItemDecoration(getActivity (),DividerItemDecoration.VERTICAL));
+        init();
         return newsView;
     }
 
-    class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.MyHolder> {
-        private Context context;
-        private ArrayList<ListItem> list = new ArrayList<>();    // 新闻列表数据
+    public void init(){
+        dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.CHINA);
+        initList();
 
-        public TitleAdapter(Context context, ArrayList<ListItem> list){
+    }
+
+    class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.MyHolder> {
+        private Context context;// 新闻列表数据
+
+        public TitleAdapter(Context context){
             this.context = context;
-            this.list = list;
         }
 
         @NonNull
@@ -68,16 +81,40 @@ public class NewsFragment extends Fragment {
         @Override
         public void onBindViewHolder(@NonNull MyHolder holder, int position) {
             LogUtils.debug(String.valueOf(position));
-            ListItem item = list.get(position);
-            holder.title.setText(item.getTitle());
-            holder.content.setText(item.getContent());
-            holder.date.setText(item.getDate());
-            Glide.with(context).load(item.getImgUrl()).into(holder.imgUrl);
+            holder.title.setText(newsBeanList.get(position).getTitle());
+            holder.content.setText(newsBeanList.get(position).getDesc());
+            holder.date.setText(dateFormat.format(newsBeanList.get(position).getPublishTime()));
+            setImage(holder,position);
+//            try {
+//                URL url = new URL(newsBeanList.get(position).getImageUrl());
+//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                //使用GET方法访问网络
+//                connection.setRequestMethod("GET");
+//                //超时时间为10秒
+//                connection.setConnectTimeout(5000);
+//
+//                int code = connection.getResponseCode();
+//                if (code == 200) {
+//                    InputStream inputStream = connection.getInputStream();
+//                    //使用工厂把网络的输入流生产Bitmap
+//                    BitmapFactory.Options options=new BitmapFactory.Options();
+//                    options.inSampleSize = 10;
+//                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream,null,options);
+//                    holder.imgUrl.setImageBitmap(bitmap);
+//                    inputStream.close();
+//                }else {
+//                    throw new RuntimeException("获取失败");
+//                }
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+
+//            Glide.with(context).load(item.getImgUrl()).into(holder.imgUrl);
         }
 
         @Override
         public int getItemCount() {
-            return list.size();
+            return newsBeanList.size();
         }
 
         public class MyHolder extends RecyclerView.ViewHolder {
@@ -98,12 +135,46 @@ public class NewsFragment extends Fragment {
 
     // 初始化新闻列表数据（假数据）
     public void initList() {
-        for(int i=0;i<6;i++){
-            ListItem item = new ListItem("我是标题","我是内容","2021-10-27",R.drawable.fzu4);
-            list.add(item);
+        Result result = new Result();
+        try {
+            result = HttpUtils.GetRequest("/news/get");
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        LogUtils.debug(String.valueOf(list.size()));
+        newsBeanList = JSONArray.parseArray(result.getData().toString(), NewsBean.class);
     }
 
 
+    public void  setImage(TitleAdapter.MyHolder holder, int pos){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(newsBeanList.get(pos).getImageUrl());
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    //使用GET方法访问网络
+                    connection.setRequestMethod("GET");
+                    //超时时间为10秒
+                    connection.setConnectTimeout(5000);
+
+                    int code = connection.getResponseCode();
+                    if (code == 200) {
+                        InputStream inputStream = connection.getInputStream();
+                        //使用工厂把网络的输入流生产Bitmap
+                        BitmapFactory.Options options=new BitmapFactory.Options();
+                        options.inSampleSize = 4;
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream,null,options);
+                        holder.imgUrl.setImageBitmap(bitmap);
+                        inputStream.close();
+                    }else {
+                        throw new RuntimeException("获取失败");
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+    }
 }

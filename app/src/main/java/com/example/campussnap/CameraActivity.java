979b57d0444181c9cd5016ee4dceb66bcfb4fce6
2545163122@qq.com <OoooK12138>
2021-12-15 20:49:49 +0800

    拍照定位

--- app/src/main/java/com/example/campussnap/CameraActivity.java
+++ app/src/main/java/com/example/campussnap/CameraActivity.java
@@ -3,42 +3,110 @@ package com.example.campussnap;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.app.Activity;
+import android.os.Bundle;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.example.campussnap.MainActivity;
+import com.example.campussnap.R;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class CameraActivity extends AppCompatActivity {
 
-public class CameraActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView photoView;
     private Button upLoadBtn;
     private Uri imageURL;
 
     private String photoPath;
+    private TextView locationInfo;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_camera);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.evaluate_title_bar);
-
-        initView();
-    }
-
-
-    private void initView(){
         upLoadBtn = findViewById(R.id.up_load);
         photoView = findViewById(R.id.photo_item);
-        photoView.setOnClickListener(this);
+        photoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, 2);
+            }
+        });
+
+        locationInfo=findViewById(R.id.locationInfo);
+        locationInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(CameraActivity.this,LocationActivity.class);
+                Bundle bundle = new Bundle();
+                intent.putExtras(bundle);
+                startActivityForResult(intent,888);
+            }
+        });
+
     }
 
+
+
+
+
+
+
+
+
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 2) {
             // 从相册返回的数据
@@ -48,17 +116,18 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 photoView.setImageURI(imageURL);
             }
         }
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.photo_item:
-                Intent intent = new Intent(Intent.ACTION_PICK, null);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, 2);
-                break;
+        if (requestCode==888){
+            Bundle bundle = data.getExtras();
+            String str =bundle.getString("aaa");
+//           Log.v("aaaaa",str);
+            locationInfo.setText(str);
         }
     }
+
+
+
+
+
+
+
 }

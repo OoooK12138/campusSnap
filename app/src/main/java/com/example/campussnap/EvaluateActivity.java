f104884199137062bc9377fe1349e6fcb756871d
daytoykkk <411487840@qq.com>
2021-12-15 19:52:21 +0800

    feat:完成发表评价界面的静态界面和逻辑绑定

--- app/src/main/java/com/example/campussnap/EvaluateActivity.java
+++ app/src/main/java/com/example/campussnap/EvaluateActivity.java
@@ -1,22 +1,82 @@
 package com.example.campussnap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
-public class EvaluateActivity extends AppCompatActivity {
+import com.example.campussnap.bean.CommentBean;
+
+//发表评价界面
+public class EvaluateActivity extends Activity implements View.OnClickListener{
+
+    private ImageView back_btn;
+    private Button comment_btn;
+    private EditText comment_input;
+    private RatingBar result_ratingBar;
+    private RatingBar speed_ratingBar;
+
+    private CommentBean postData = new CommentBean();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_evaluate);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.evaluate_title_bar);
 
-        /**
-         * 跳转参数传入
-         */
-        Intent intent=getIntent();
-        Bundle bundle=intent.getExtras();
-//        System.out.println(bundle.get("position"));
+        init();
+    }
+
+    // 初始化控件
+    private void init() {
+        back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(this);
+        comment_btn = findViewById(R.id.comment_btn);
+        comment_btn.setOnClickListener(this);
+        comment_input = findViewById(R.id.comment_input);
+        comment_input.setOnClickListener(this);
+        result_ratingBar = findViewById(R.id.result_ratingBar);
+        result_ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean b) {
+                postData.setRatingResult((int)rating);
+            }
+        });
+        speed_ratingBar = findViewById(R.id.speed_ratingBar);
+        speed_ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean b) {
+                postData.setRatingSpeed((int)rating);
+            }
+        });
+    }
+
+    public void postComment(){
+        String comment = comment_input.getText().toString();
+        if(comment.equals("")){
+            Toast.makeText(EvaluateActivity.this, "评价内容不能为空！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        postData.setCommend(comment);
+        Log.v("mybug", postData.toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        int position = view.getId();
+        switch (position) {
+            case R.id.back_btn:         // 返回
+                break;
+            case R.id.comment_btn:      // 发表评论
+                postComment();
+                break;
+        }
     }
 }
